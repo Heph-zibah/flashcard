2e16ca4706fc9ee91c93a56860b81de6dc8e00a2
oaDaramola <oluwatosinhephzibah@gmail.com>
2025-09-18 13:57:42 +0100

    added the button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import FlashcardForm from "@/components/custom/FlashcardForm";
 import FlashcardList from "@/components/custom/FlashcardList";
 import { useFlashcards } from "@/hooks/useFlashcards";
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   const {
     flashcards,
@@ -17,6 +17,7 @@ export default function Home() {
     addFlashcard,
     shuffle,
     loading,
+    clearFlashcards,
   } = useFlashcards([
     {
       id: 1,
@@ -48,13 +49,22 @@ export default function Home() {
         toggleLearned={toggleLearned}
         loading={loading}
       />
-
-      <button
-        onClick={shuffle}
-        className="mt-10 bg-yellow-500 text-white px-6 py-2 rounded-xl hover:bg-yellow-600 transition shadow-md font-medium"
-      >
-        Shuffle Flashcards
-      </button>
+      <div className="flex flex-col md:flex-row items-center mt-10">
+        <Button
+          variant="destructive"
+          onClick={clearFlashcards}
+          className="bg-red-500 text-white px-4 py-2 rounded "
+        >
+          Clear All Flashcards
+        </Button>
+        <Button
+          variant="default"
+          onClick={shuffle}
+          className=" bg-yellow-500 text-white px-6 py-2 rounded-xl hover:bg-yellow-600 transition shadow-md font-medium"
+        >
+          Shuffle Flashcards
+        </Button>
+      </div>
     </main>
   );
 }
--- src/components/custom/FlashcardList.tsx
+++ src/components/custom/FlashcardList.tsx
@@ -21,7 +21,7 @@ export default function FlashcardList({
 }: FlashcardListProps) {
   if (loading) {
     return (
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl">
         {[...Array(6)].map((_, i) => (
           <div key={i} className="p-4 bg-white rounded-xl shadow-md">
             <Skeleton className="h-6 w-3/4 mb-3" />
